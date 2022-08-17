7521c21ddb233fa80491491cef9308850a01549e
Smarty07s <111247057+Smarty07s@users.noreply.github.com>
2022-08-17 07:26:06 +0530

    Updatd

--- code/modules/setup.py
+++ code/modules/setup.py
@@ -1,4 +1,4 @@
 from setuptools import setup
 
-setup(name="mda", version="1.5", description="ease", long_description="Makes coding easy!", packages=[
+setup(name="mda", version="1.5.2", description="ease", long_description="Makes coding easy!", packages=[
       "mda"], author="Smarty", platforms="All", author_email="soumyamishra2009@outlook.com", requires=["math"])
